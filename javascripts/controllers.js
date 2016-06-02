293c788d56281e46f3ce33d3bcc1497bfe3edebe
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-02 13:25:54 -0500

    change to getMovie

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -10,7 +10,7 @@ angular.module("app")
   .controller("AddCtrl", function(omdbFactory, firebaseFactory, $location) {
     const add = this;
     add.addMovie = title => {
-      const omdbData = omdbFactory.getMovieData(title);
+      const omdbData = omdbFactory.getMovie(title);
 
       firebaseFactory.addMovie({
         Title: omdbData.Title,
